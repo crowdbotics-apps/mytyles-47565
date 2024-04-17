989c6134c7daf4650c46d98e966a37d944f2b62d
Crowdbotics <team@crowdbotics.com>
2024-04-17 13:16:22 +0000

    Initial commit

---
+++ backend/mytyles_47565/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mytyles_47565 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "mytyles_47565.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mytyles-47565",
+  url: "https://mytyles-47565.botics.co",
+  api: "https://mytyles-47565.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
